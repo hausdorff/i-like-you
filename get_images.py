474bba84618290dbd9ea6087f0fda4c850324b5e
Alex Clemmer <clemmer.alexander@gmail.com>
2013-06-20 17:26:03 -0400

    Add code to break data into cross-validation segments

--- get_images.py
+++ get_images.py
@@ -1,5 +1,5 @@
 from __future__ import with_statement
-import urllib, os, time
+import urllib, os, time, random
 from bs4 import BeautifulSoup
 from collections import defaultdict
 
@@ -56,23 +56,38 @@ def iter_thru_valid_divs (raw):
         if img_src != None:
             yield img_src, countmap
 
+def cv_line_nums (li):
+    random.shuffle(li)
+    perc = int(0.1*len(li))
+    for i in xrange(0, len(li), perc):
+        yield li[i:i+perc], li[0:i] + li[i+perc:]
+
+def write_data (lines, ident, datatype):
+    with open("%s_%s_%d.txt"% (DATA_LABELS, datatype, ident), 'wb') as w:
+        w.write("id\tinto you\tnot into you\tverdict still out\n")
+        for line in lines:
+            w.write(line)
+
 def setup_data ():
     setup_env()
     raw = get_data()
 
-    with open(DATA_LABELS, 'wb') as labelswriter:
-        labelswriter.write("id\tinto you\tnot into you\tverdict still out\n")
-        for i,(url,labels) in enumerate(iter_thru_valid_divs(raw)):
-            print 'processing file %d' % i
-            filename = "%stxt_msg%d.jpg" % (IMG_DIR, i)
+    labels_file_lines = []
+    for i,(url,labels) in enumerate(iter_thru_valid_divs(raw)):
+        print 'processing file %d' % i
+        filename = "%stxt_msg%d.jpg" % (IMG_DIR, i)
 
-            with open(filename, 'wb') as w:
-                w.write(urllib.urlopen(url).read())
-                time.sleep(0.25)
-                labelswriter.write("%d\t%d\t%d\t%d\n" %
-                                   (i, labels[INTO_YOU],
-                                    labels[NOT_INTO_YOU],
-                                    labels[VERDICT_STILL_OUT]))
+        with open(filename, 'wb') as w:
+            w.write(urllib.urlopen(url).read())
+            time.sleep(0.25)
+            labels_file_lines.append("%d\t%d\t%d\t%d\n" %
+                                     (i, labels[INTO_YOU],
+                                      labels[NOT_INTO_YOU],
+                                      labels[VERDICT_STILL_OUT]))
+
+    for i,(train_chunk,test_chunk) in enumerate(cv_line_nums(labels_file_lines)):
+        write_data(train_chunk, i, 'train')
+        write_data(test_chunk, i, 'test')
 
 
 if __name__ == '__main__':
